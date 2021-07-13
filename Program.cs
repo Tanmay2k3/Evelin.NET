25234dc7ca95afbd3e0265260c8e087813271efd
Tanmay2k3 <41178950+Tanmay2k3@users.noreply.github.com>
2021-07-13 10:35:54 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@
                         MessageCacheSize = 200,
                     };
 
-                    config.Token = context.Configuration[secrets.TOKEN];
+                    config.Token = context.Configuration[process.env.TOKEN];
                 })
                 .UseCommandService((context, config) =>
                 {
